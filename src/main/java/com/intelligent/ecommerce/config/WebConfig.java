a307dda1666919057cf9c80bcdef80325bdb63a2
Hongze Wang <bgwy666@gmail.com>
2024-10-19 23:34:30 +1100

    [add] Add CORS WebConfig.java

---
+++ src/main/java/com/intelligent/ecommerce/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.intelligent.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public WebConfig() {
+        System.out.println("WebConfig is loaded!");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+    }
+}
